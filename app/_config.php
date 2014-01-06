dff16bdfc8cd04e1318f6bc0156b1295d6636b2e
Morven Lewis-Everley <morven@i-lateral.com>
2014-01-06 15:14:56 +0000

    Update to using yml for extensions

--- app/_config.php
+++ app/_config.php
@@ -11,8 +11,5 @@ require_once('conf/ConfigureFromEnv.php');
 // Set the site locale
 i18n::set_locale('en_GB');
 
-// Add extensions
-Object::add_extension('SiteConfig', 'AppSiteConfig');
-
 // Set quality of image rendering
 GD::set_default_quality(80);
